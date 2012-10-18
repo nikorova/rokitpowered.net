cb71528555c39bc5b3388ecac9f3ba534309903d
nikorova <oneofy@gmail.com>
2012-10-18 13:44:32 -0500

    doin it live

--- web/index.php
+++ web/index.php
@@ -38,52 +38,52 @@ $app->get('/', function() use ($app) {
 
 	$aids = array (
 		"1"	=> array( 
-			"href" 		=> "",
-			"caption"	=> "",
-			"blurb" 	=> ""
+			"href" 		=> "http://silex.sensiolabs.org",
+			"caption"	=> "Silex - Sexy, Lightweight MVC Framework",
+			"blurb" 	=> "You can bet that if I ever meet Fabien I'll be buying him a beer."
 		),
 		"2"	=> array( 
-			"href" 		=> "",
-			"caption"	=> "",
-			"blurb" 	=> ""
+			"href" 		=> "http://doctrine.sensiolabs.org",
+			"caption"	=> "Doctrine - ORM and DBAL For You and Me",
+			"blurb" 	=> "Why punch yourself in the head until the pretty lights come when you can just use Doctrine?"
 		),
 		"3"	=> array( 
-			"href" 		=> "",
-			"caption"	=> "",
-			"blurb" 	=> ""
+			"href" 		=> "http://twig.sensiolabs.org",
+			"caption"	=> "Twig - Robust Templating Engine",
+			"blurb" 	=> "I may be a tad spoiled. I blame Sensio."
 		),
 		"4"	=> array( 
-			"href" 		=> "",
-			"caption"	=> "",
-			"blurb" 	=> ""
+			"href" 		=> "http://phptherightway",
+			"caption"	=> "PHP The Right Way - Because 'The Wrong Way' is a Thing",
+			"blurb" 	=> "When was the last time you googled the phrase 'best practice'?"
 		),
 	);
 
 	$likes= array (
 		"1"	=> array( 
-			"href" 		=> "",
-			"caption"	=> "",
-			"blurb" 	=> ""
+			"href" 		=> "http://schneier.com",
+			"caption"	=> "Schneier on Security - Your Brain on Security",
+			"blurb" 	=> "Known to be fond of squid and cryptography."
 		),
 		"2"	=> array( 
-			"href" 		=> "",
-			"caption"	=> "",
-			"blurb" 	=> ""
+			"href" 		=> "http://css-tricks.com",
+			"caption"	=> "CSS Tricks - Chris Coyier Show Us His Kung Fu",
+			"blurb" 	=> "It's probably better than yours."
 		),
 		"3"	=> array( 
-			"href" 		=> "",
-			"caption"	=> "",
-			"blurb" 	=> ""
+			"href" 		=> "http://codinghorror.com",
+			"caption"	=> "Coding Horror - Stare Into the Abyss",
+			"blurb" 	=> "Jeff Atwood has a beautiful, pragmatic way of looking at our craft."
 		),
 		"4"	=> array( 
-			"href" 		=> "",
-			"caption"	=> "",
-			"blurb" 	=> ""
+			"href" 		=> "http://krebsonsecurity.com",
+			"caption"	=> "KrebsOnSecurity - InfoSec a la Brian Krebs",
+			"blurb" 	=> "Smart fella. Seems to like tracking down and tearing up botnets.  Not a bad way to spend a Tuesday."
 		),
 	);
 
 	$content = array(
-		"byLine" 	=> "~ a brief yet triumphant exhibition ~",
+		"byLine" 	=> "a brief yet triumphant exhibition",
 		"navigation" 	=> $navigation, 
 		"aids" 		=> $aids, 
 		"likes" 	=> $likes
