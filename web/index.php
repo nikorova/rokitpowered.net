aece53fcdd7312c33d7c1daabc8771a5ff599f23
nikorova <oneofy@gmail.com>
2012-10-18 11:49:32 -0500

    learning how to use git oh lawd

--- web/index.php
+++ web/index.php
@@ -4,13 +4,91 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../options.inc.php';
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application();
 
 // who loves bugs? you know i do
 $app['debug'] = $env['debug'];
 
-$app->get('/hello/{name}', function($name) use ($app) {
-    return 'Hello there '. $app->escape($name);
+// register providers
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__ . '/views/',
+));
+
+$app->get('/', function() use ($app) {
+	$navigation = array(
+		"1" => array(
+			"href" 		=> "http://rokitpowered.net",
+			"caption"	=> "You Are Here"
+		),
+		"2" => array( 
+			"href" 		=> "http://twitter.com/enlore",
+			"caption" 	=> "@enlore"
+		),
+		"3" => array( 
+			"href" 		=> "http://github.com/nikorova",
+			"caption" 	=> "Oh My Github!"
+		),
+		"4" => array( 
+			"href" 		=> "http://meetup.com/Clarksville-Tech-Dev-Meet",
+			"caption" 	=> "Clarksville Tech & Dev Meetup"
+		),
+	);
+
+	$aids = array (
+		"1"	=> array( 
+			"href" 		=> "",
+			"caption"	=> "",
+			"blurb" 	=> ""
+		),
+		"2"	=> array( 
+			"href" 		=> "",
+			"caption"	=> "",
+			"blurb" 	=> ""
+		),
+		"3"	=> array( 
+			"href" 		=> "",
+			"caption"	=> "",
+			"blurb" 	=> ""
+		),
+		"4"	=> array( 
+			"href" 		=> "",
+			"caption"	=> "",
+			"blurb" 	=> ""
+		),
+	);
+
+	$likes= array (
+		"1"	=> array( 
+			"href" 		=> "",
+			"caption"	=> "",
+			"blurb" 	=> ""
+		),
+		"2"	=> array( 
+			"href" 		=> "",
+			"caption"	=> "",
+			"blurb" 	=> ""
+		),
+		"3"	=> array( 
+			"href" 		=> "",
+			"caption"	=> "",
+			"blurb" 	=> ""
+		),
+		"4"	=> array( 
+			"href" 		=> "",
+			"caption"	=> "",
+			"blurb" 	=> ""
+		),
+	);
+
+	$content = array(
+		"byLine" 	=> "~ a brief yet triumphant exhibition ~",
+		"navigation" 	=> $navigation, 
+		"aids" 		=> $aids, 
+		"likes" 	=> $likes
+	);
+	return $app['twig']->render('index.twig', $content); 
 });
 
 $app->run();
